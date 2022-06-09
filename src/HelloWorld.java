40fdb11e1988b0c38caaa24bae33488cf4231ba5
Justin Welsh <justinwelsh33@gmail.com>
2022-06-09 15:14:58 -0500

    notes from lecture commented out

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,49 @@
-public class HelloWorld {
-    public static void main (String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+
+//NOTES FROM LEC:
+// capital case for method/function names.
+// use camel case for variables
+
+//public class HelloWorld {
+//    public static void main (String[] args) {
+////        Single line comment
+//        /*
+//         *  Multi-line comment :)
+//         */
+//
+//
+//        byte tinyNum = 127;
+//        tinyNum++;
+//
+//        int num = 235000;
+//
+//        long id = 1L;
+//
+//        System.out.println("Hello, World!" + "!"); // print single line to the console/ you con concatenate
+//        System.out.printf("Hello, %s\n ", "Justin!"); // ''%s' similar to temp. strings '${}'
+//        System.out.println(id);
+//
+//        float floater = 8.35F; // 'F' cast to a float
+//        float floater2 = (float)8.35F;
+//        float noDecimal = 9;
+//
+//        System.out.println(floater);
+//        System.out.println(noDecimal);
+//
+//        char charmander = '&';
+//        System.out.println(charmander);
+//
+//        String message = "\tI really like marshmallows"; // '\t' tabs in the console?
+//        System.out.println(message);
+//
+//
+//        boolean doingJavaRightNow = true;
+//        if (doingJavaRightNow == true) {
+//            System.out.println("wow, i am cool");
+//        }; // no need to use
+//        //strict equal operator for comparison '===', since the datatype is already implied in the naming of the variables
+//
+//
+//    }
+//}
+
+
