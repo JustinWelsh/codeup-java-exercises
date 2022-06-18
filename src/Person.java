b8c6ebc70f95fe6093b36cd13a6b893af250fd31
Justin Welsh <justinwelsh33@gmail.com>
2022-06-17 21:13:38 -0500

    updated

--- src/Person.java
+++ src/Person.java
@@ -1,24 +1,52 @@
 public class Person {
-    private String name;
+    private String name;        //No value is set because we will set it when we instantiate a 'new' Person()
 
-    public String getName(){
-//TODO: return the person's name
-        return this.name;
+    public String getName() {
+        return this.name;           //TODO: return the person's name
+
     }
 
-    public String setName(String name){
-//TODO: change the name field to the passed value
-        return this.name = name;
+    public void setName(String name) {
+        this.name = name;           //TODO: change the name field to the passed value
+
     }
-    public void sayHello(){
-//TODO: print a message to the console using the person's name
-        System.out.printf("Hello %s", name);
+
+    public void sayHello() {
+        System.out.printf("Hello %s", name);        //TODO: print a message to the console using the person's name
+    }
+
+                        //The class should have a constructor that accepts a `String` value and sets
+                        //the person's name to the passed string.
+                        //
+                        //Create a `main` method on the class that creates a new `Person` object and
+                        //tests the above methods.
+
+
+    //***  CONSTRUCTORS  ***
+    public Person(String name) {
+        //This constructor has parameters.
+        //Method-overloading is a good thing to catch diversified inputs.
+        this.name = name;
+    }
+
+    public Person() {
+        //This is an 'empty' constructor.
+        // when creating an object this way use No arguments.
+        //will need to manually set values by using dot notation.
     }
 
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("Justin");
-        person.sayHello();
-    }
+        Person Justin = new Person("Justin");       //instantiated a new person!
 
+        Person Marty = new Person("Marty");         //instantiated a new person!
+        Marty.setName("Marty test");
+        System.out.println(Justin.getName());
+        System.out.println(Marty.getName());
+
+        Person Mike = new Person();
+        Mike.setName("Mike");
+        System.out.println(Mike.getName());
+        Marty.sayHello();
+
+    }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,18 @@
-package util;
-
-public class Input {
-}
+//package util;
+//
+//import java.util.Scanner;
+//
+//public class Input {
+//    private Scanner scanner = new Scanner(System.in);
+//
+//    String getString() {
+//        System.out.println("Give me a string!");
+//        return scanner.nextLine();
+//    }
+//
+//    boolean yesNo() {
+//        System.out.println("Yes Or No?");
+//        String userInput = scanner.next();
+//    }
+//
+//}
