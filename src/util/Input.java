f9220e3e51f2ccffcc6452913b1d104b262ce97d
Justin Welsh <justinwelsh33@gmail.com>
2022-06-18 21:02:12 -0500

    Completed the input validaation class

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,18 +1,57 @@
-//package util;
-//
-//import java.util.Scanner;
-//
-//public class Input {
-//    private Scanner scanner = new Scanner(System.in);
-//
-//    String getString() {
-//        System.out.println("Give me a string!");
-//        return scanner.nextLine();
-//    }
-//
-//    boolean yesNo() {
-//        System.out.println("Yes Or No?");
-//        String userInput = scanner.next();
-//    }
-//
-//}
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner = new Scanner(System.in);       //I thought new instances needed to be the same name as the class?
+
+    public String getString() {
+        System.out.println("Enter a string");
+        return scanner.nextLine();
+    }
+
+
+    public boolean yesNo() {
+        System.out.println("yes or no?");
+        String userInput = scanner.nextLine();
+        if (userInput.equalsIgnoreCase("y") ||
+            userInput.equalsIgnoreCase("yes") ||
+            userInput.equalsIgnoreCase("yeah") ||
+            userInput.equalsIgnoreCase("ye") ||
+            userInput.equalsIgnoreCase("ya")) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public int getInt(int min, int max) {
+        System.out.printf("Enter a number between %d - %d.\n", min, max);
+        int userNum = scanner.nextInt();
+        if (userNum >= min && userNum <= max) {
+            return userNum;
+        }
+        return getInt(min, max);
+    }
+
+     public int getInt() {
+         System.out.printf("Enter a number\n");
+         int userNum = scanner.nextInt();
+         return userNum;
+    }
+    public double getDouble(double min, double max) {
+        System.out.printf("Enter a large decimal number between %f - %f\n", min, max);
+        double userNum = scanner.nextDouble();
+        if (userNum >= min && userNum <= max) {
+            return userNum;
+        } return getDouble(min, max);
+
+    }
+
+    public double getDouble() {
+        System.out.printf("Enter a decimal number before I turn green. .\n");
+        double userNum = scanner.nextDouble();
+        return userNum;
+    }
+
+}
---
+++ src/util/InputTest.java
@@ -0,0 +1,20 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input in = new Input();     //Why are we needing to do this?
+
+
+//        System.out.printf("You entered \"%s\" ;)", in.getString());
+
+//        System.out.println(in.yesNo());
+
+//        System.out.printf("%d is a good number!", in.getInt(1,10));
+
+//        System.out.printf("%d is a mighty fine number, good choice!", in.getInt());
+
+//        System.out.println(in.getDouble(1,20));
+
+        System.out.printf("%f , nice! worth the wait!", in.getDouble());
+    }
+}
