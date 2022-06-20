9dbcaf31f21c1a8a1a3444c37a053eb4def58e8e
Justin Welsh <justinwelsh33@gmail.com>
2022-06-19 21:20:45 -0500

    OOP exercise completed

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -3,7 +3,7 @@ import util.Input;
 
 public class CircleApp {
     public static void main(String[] args) {
-        Input in = new Input();
+        Input in = new Input();     //instantiated so we can access the methods from the "Input" class.
         System.out.println("Lets make a circle.\n");
         Circle circle = new Circle(in.getDouble());
         System.out.printf("Circumference = %f ", circle.getCircumference());
