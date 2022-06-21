337fb8ba4e6ddca411101107eec0a8ab630d23fe
Justin Welsh <justinwelsh33@gmail.com>
2022-06-21 11:37:30 -0500

    almost complete

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,24 @@
 package shapes;
 
-public class Rectangle {
-    protected int length;
-    protected int width;
+//public class Rectangle {
+//    protected int length;
+//    protected int width;
+//
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public int getPerimeter() {
+//        return 2 * this.length + 2 * this.width;
+//    }
+//
+//    public int getArea() {
+//        return this.length * this.width;
+//    }
+//}
 
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
 
-    public int getPerimeter() {
-        return 2 * this.length + 2 * this.width;
-    }
-
-    public int getArea() {
-        return this.length * this.width;
-    }
-}
+//public class Rectangle extends Quadrilateral implements Measurable {
+//
+//}
